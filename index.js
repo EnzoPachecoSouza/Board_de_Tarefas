9736b1b388fb170b41c28b98b08206e7a2f88f17
Enzo Pacheco <enzo.pach@gmail.com>
2025-01-10 23:53:25 -0300

    Finalizando a Logica e iniciando a estilização

--- index.js
+++ index.js
@@ -1,160 +1,37 @@
-// const tasks = [
-//     { nome: "Criar tela de Login", etiqueta: "Front-end", data: "Criado em: 21/08/2024" },
-//     { nome: "Criar tela de Cadastro", etiqueta: "Front-end", data: "Criado em: 21/08/2024" },
-//     { nome: "Criar Banco de Dados", etiqueta: "Back-end", data: "Criado em: 21/08/2024" },
-// ]
-
-// function gravarDadosLocalStorage(taskObj) {
-//     let tasksSalves = JSON.parse(localStorage.getItem("tasks"));
-//     tasksSalves.push(taskObj);
-
-//     localStorage.setItem("tasks", JSON.stringify(tasksSalves));
-// }
-
-// function carregarDadosLocalStorage() {
-//     console.log("task", JSON.parse(localStorage.getItem("tasks")));
-// }
-
-// const form = document.getElementById("create-board-task-form");
-
-// form.addEventListener("submit", function (evento) {
-//     evento.preventDefault();
-//     createTask();
-// });
-
-// function createTask() {
-//     console.log("Tarefa criada com sucesso!");
-//     const nameTask = document.getElementById("nameTask");
-//     const etiqueta = document.getElementById("etiqueta");
-//     const data = "Criado em: 21/08/2024"
-
-//     const titleTask = document.createElement("h2");
-//     titleTask.classList.add("title-task");
-//     titleTask.innerHTML = nameTask.value;
-//     // titleTask.innerHTML = task.nome;
-
-//     const etiquetaTask = document.createElement("p");
-//     etiquetaTask.classList.add("etiqueta-task");
-//     etiquetaTask.innerHTML = etiqueta.value;
-//     // etiquetaTask.innerHTML = task.etiqueta;
-
-//     const dataTask = document.createElement("p");
-//     dataTask.classList.add("date-task");
-//     dataTask.innerHTML = "Criado em: 21/08/2024";
-//     // dataTask.innerHTML = task.data;
-
-//     const li = document.createElement("li");
-//     li.classList.add("card-task");
-
-//     const btnConcluir = document.createElement("button");
-//     btnConcluir.addEventListener("click", concluirTask);
-//     btnConcluir.type = "button";
-//     btnConcluir.classList.add("btn-concluir");
-//     btnConcluir.innerHTML = "Concluir";
-
-//     const ul = document.getElementById("list-task");
-//     ul.appendChild(li);
-//     li.appendChild(titleTask);
-//     li.appendChild(etiquetaTask);
-//     li.appendChild(dataTask);
-//     li.appendChild(btnConcluir);
-
-//     const taskObj = {
-//         nome: nameTask.value,
-//         etiqueta: etiqueta.value,
-//         data: data
-//     }
-
-//     gravarDadosLocalStorage(taskObj);
-//     carregarDadosLocalStorage();
-// }
-
-// function renderizarTarefas() {
-//     const renderizar = JSON.parse(localStorage.getItem("tasks"));
-//     renderizar.map((task) => {
-//         console.log("Renderizar Tarefa", task.nome, task.etiqueta, task.data);
-//         const titleTask = document.createElement("h2");
-//         titleTask.classList.add("title-task");
-//         // titleTask.innerHTML = nameTask.value;
-//         titleTask.innerHTML = task.nome;
-
-//         const etiquetaTask = document.createElement("p");
-//         etiquetaTask.classList.add("etiqueta-task");
-//         // etiquetaTask.innerHTML = etiqueta.value;
-//         etiquetaTask.innerHTML = task.etiqueta;
-
-//         const dataTask = document.createElement("p");
-//         dataTask.classList.add("date-task");
-//         // dataTask.innerHTML = "Criado em: 21/08/2024";
-//         dataTask.innerHTML = task.data;
-
-//         const li = document.createElement("li");
-//         li.classList.add("card-task");
-
-//         const btnConcluir = document.createElement("button");
-//         btnConcluir.addEventListener("click", concluirTask);
-//         btnConcluir.type = "button";
-//         btnConcluir.classList.add("btn-concluir");
-//         btnConcluir.innerHTML = "Concluir";
-
-//         const ul = document.getElementById("list-task");
-//         ul.appendChild(li);
-//         li.appendChild(titleTask);
-//         li.appendChild(etiquetaTask);
-//         li.appendChild(dataTask);
-//         li.appendChild(btnConcluir);
-//     })
-// }
-
-// function concluirTask(event) {
-//     alert("Tarefa concluída com sucesso!");
-
-//     // Obter o botão que disparou o evento
-//     const btnConcluir = event.target;
-
-//     // Obter o elemento da tarefa (li) que contém o botão
-//     const cardTask = btnConcluir.parentElement;
-
-//     // Substituir o botão por um emote
-//     const emote = document.createElement("span");
-//     emote.classList.add("emote-concluido");
-//     emote.innerHTML = "✅"; // Substitua pelo emote que você tem salvo
-
-//     // Remover o botão e adicionar o emote
-//     cardTask.removeChild(btnConcluir);
-//     cardTask.appendChild(emote);
-
-//     // Adicionar a classe de estilo ao título da tarefa
-//     const titleTask = cardTask.querySelector(".title-task");
-//     titleTask.classList.add("title-concluido");
-
-//     countTask++;
-// }
-
-// window.onload = function() {
-//     renderizarTarefas();
-// };
-
 //DECLARAÇÃO DAS TAREFAS INICIAIS
 //Aqui foi criado um ARRAY/LIST com os OBJETOS que representam as tarefas iniciais, e cada OBJETO
 //contém: NOME, ETIQUETA e DATA
 
 const tasks = [
-    { nome: "Criar tela de Login", etiqueta: "Front-end", data: "Criado em: 21/08/2024" },
-    { nome: "Criar tela de Cadastro", etiqueta: "Front-end", data: "Criado em: 21/08/2024" },
-    { nome: "Criar Banco de Dados", etiqueta: "Back-end", data: "Criado em: 21/08/2024" },
+    { nome: "Criar tela de Login", etiqueta: "Front-end", data: "Criado em: 21/08/2024", check: false},
+    { nome: "Criar tela de Cadastro", etiqueta: "Front-end", data: "Criado em: 21/08/2024", check: false},
+    { nome: "Criar Banco de Dados", etiqueta: "Back-end", data: "Criado em: 21/08/2024", check: false},
 ];
 
 //-----------------------------------------------------------------------------------------------
 
 //FUNÇÃO DE GRAVAR DADOS NO LOCAL STORAGE (armazenamento de dados via WEB)
-//
+//let tasksSalvas = JSON.parse(localStorage.getItem("tasks")) || [];
+//Ele irá tentar carregar o array de tarafas salvo no LOCAL STORAGE
+//Caso não exista, ele cria um Array VAZIO como Padrão
+
+//tasksSalvas.push(taskObj);
+//Adiciona a nova tarefa ao array de Tasks Salvas
+
+//localStorage.setItem("tasks", JSON.stringify(tasksSalvas));
+//Salva o Array atualizado no LOCALSTORAGE em formado de string, usando o JSON.strinfy
 
 function gravarDadosLocalStorage(taskObj) {
     let tasksSalvas = JSON.parse(localStorage.getItem("tasks")) || [];
     tasksSalvas.push(taskObj);
     localStorage.setItem("tasks", JSON.stringify(tasksSalvas));
 }
+
+//---------------------------------------------------------------------------------------------
+
+//FUNÇÃO DE CARREGAR DADOS DO LOCAL STORAGE
+
+//Carrega as tarefas salvas no localStorage, converte de string para objeto com JSON.parse e exibe no console.
 
 function carregarDadosLocalStorage() {
     console.log("Tasks:", JSON.parse(localStorage.getItem("tasks")));
@@ -177,6 +54,7 @@ function createTask() {
         nome: nameTask,
         etiqueta: etiqueta,
         data: data,
+        check: false
     };
 
     addTaskToDOM(taskObj);
@@ -185,7 +63,18 @@ function createTask() {
 }
 
 function addTaskToDOM(task) {
+    const divDescricao = document.createElement("div");
+    divDescricao.classList.add("task-descricao");
+
+    const divDescricaoAbaixo = document.createElement("div");
+    divDescricaoAbaixo.classList.add("descricao-abaixo");
+
     const titleTask = document.createElement("h2");
+    if(!task.check){
+        titleTask.classList.add("title-task");
+    }else{
+        titleTask.classList.add("title-concluido");
+    }
     titleTask.classList.add("title-task");
     titleTask.innerHTML = task.nome;
 
@@ -199,24 +88,48 @@ function addTaskToDOM(task) {
 
     const li = document.createElement("li");
     li.classList.add("card-task");
-
-    const btnConcluir = document.createElement("button");
-    btnConcluir.addEventListener("click", concluirTask);
-    btnConcluir.type = "button";
-    btnConcluir.classList.add("btn-concluir");
-    btnConcluir.innerHTML = "Concluir";
-
+    
     const ul = document.getElementById("list-task");
     ul.appendChild(li);
-    li.appendChild(titleTask);
-    li.appendChild(etiquetaTask);
-    li.appendChild(dataTask);
-    li.appendChild(btnConcluir);
+    li.appendChild(divDescricao);
+    divDescricao.appendChild(titleTask);
+    divDescricao.appendChild(divDescricaoAbaixo);
+    divDescricaoAbaixo.appendChild(etiquetaTask);
+    divDescricaoAbaixo.appendChild(dataTask);
+    
+    // li.appendChild(titleTask);
+    // li.appendChild(etiquetaTask);
+    // li.appendChild(dataTask);
+    
+    
+    if(!task.check){
+        const btnConcluir = document.createElement("button");
+        btnConcluir.addEventListener("click", concluirTask);
+        btnConcluir.type = "button";
+        btnConcluir.classList.add("btn-concluir");
+        btnConcluir.innerHTML = "Concluir";
+
+        li.appendChild(btnConcluir);
+    }else{
+        const emote = document.createElement("span");
+        emote.classList.add("emote-concluido");
+        emote.innerHTML = "✅";
+
+        li.appendChild(emote);
+    }
 }
 
 function renderizarTarefas() {
+    if(JSON.parse(localStorage.getItem("tasks")) === null){
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
     const tarefasSalvas = JSON.parse(localStorage.getItem("tasks")) || [];
-    tarefasSalvas.forEach((task) => addTaskToDOM(task));
+
+    tarefasSalvas.forEach((task) => 
+        addTaskToDOM(task)
+);
+atualizarContadorTarefas();
 }
 
 function concluirTask(event) {
@@ -225,6 +138,7 @@ function concluirTask(event) {
     const btnConcluir = event.target;
     const cardTask = btnConcluir.parentElement;
 
+    // Atualizar o DOM
     const emote = document.createElement("span");
     emote.classList.add("emote-concluido");
     emote.innerHTML = "✅";
@@ -234,8 +148,36 @@ function concluirTask(event) {
 
     const titleTask = cardTask.querySelector(".title-task");
     titleTask.classList.add("title-concluido");
+
+    // Atualizar o estado no localStorage
+    const nomeTask = titleTask.innerHTML;
+    let tarefasSalvas = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tarefasSalvas = tarefasSalvas.map((task) => {
+        if (task.nome === nomeTask) {
+            return { ...task, check: true }; // Atualiza a tarefa como concluída
+        }
+        return task;
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tarefasSalvas));
+
+    atualizarContadorTarefas();
+}
+
+function atualizarContadorTarefas() {
+    const tarefasSalvas = JSON.parse(localStorage.getItem("tasks")) || [];
+    const tarefasConcluidas = tarefasSalvas.filter(task => task.check).length;
+
+    const contadorElemento = document.getElementById("completed-tasks-count");
+    contadorElemento.innerHTML = tarefasConcluidas;
+
+    localStorage.setItem("completedTasksCount", tarefasConcluidas);
 }
 
 window.onload = function () {
     renderizarTarefas();
+
+    const contadorSalvo = localStorage.getItem("completedTasksCount") || 0;
+    document.getElementById("completed-tasks-count").innerHTML = contadorSalvo;
 };
